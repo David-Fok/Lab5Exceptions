0f2f7816017b45bdd9d6a57b10c5a84117f87f58
David-Fok <foktatchun@gmail.com>
2023-10-26 10:57:34 -0600

    Add project files.

---
+++ Lab5Exceptions/Circle.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5Exceptions
+{
+    internal class Circle
+    {
+        private double radius;
+        public double Radius
+        {
+            get { return radius; }
+            set
+            {
+                if (value <= 0 || double.IsInfinity(value))
+                    throw new InvalidRadiusException("Radius cannot be negative or infinity.");
+                radius = value;
+            }
+        }
+
+        //constructor
+        public Circle(double radius) 
+        {
+          Radius = radius;
+        }
+
+        public double Area()
+        { 
+        return Math.PI * Math.Pow(Radius,2);
+        }
+
+        public override string ToString()
+        {
+            return "Area: " + this.Area();
+        }
+    }
+}
---
+++ Lab5Exceptions/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Diagnostics;
+
+namespace Lab5Exceptions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //•	One object with positive radius
+            try
+            {
+                Circle circle1 = new Circle(10);
+                Console.WriteLine(circle1);
+            }
+            catch(InvalidRadiusException ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+            //•	One object with negative radius
+            try
+            {
+                Circle circle2 = new Circle(-10);
+                Console.WriteLine(circle2);
+            }
+            catch(InvalidRadiusException ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+            //•	One object with radius of zero
+            try
+            {
+                Circle circle3 = new Circle(0);
+                Console.WriteLine(circle3);
+            }
+            catch (InvalidRadiusException ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+        }
+    }
+}
